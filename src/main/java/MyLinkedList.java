a2edde4aac5e34a61480aa3b8bc902beda01248c
b115030 <iitiangokul@gmail.com>
2020-10-30 09:42:24 +0530

    ADDED: Method and Test Cases for creating Linked List

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,6 +1,28 @@
-import javax.imageio.metadata.IIOMetadataNode;
-
 public class MyLinkedList<K> {
+    private K key;
+    private MyLinkedList next;
+
+    // constructor
+    public MyLinkedList(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public MyLinkedList getNext() {
+        return next;
+    }
+
+    public void setNext(MyLinkedList next) {
+        this.next = next;
+    }
 
     public static boolean printWelcomeMsg() {
         System.out.println("Welcome to the find maximum using generics problem.");
--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -7,5 +7,17 @@ public class MyLinkedListTest {
         boolean welcomeReturns = MyLinkedList.printWelcomeMsg();
         Assert.assertEquals(true, welcomeReturns);
     }
+
+    //UC1 test case to create simple linked list
+    @Test
+    public void givenThreeElements_whenInALinkedList_shouldPassLinkedListTest() {
+        MyLinkedList<Integer> myFirstNode = new MyLinkedList<>(56);
+        MyLinkedList<Integer> mySecondNode = new MyLinkedList<>(30);
+        MyLinkedList<Integer> myThirdNode = new MyLinkedList<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
 
