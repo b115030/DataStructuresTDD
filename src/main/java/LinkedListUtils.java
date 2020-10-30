1eb58741c62b215f3c11056cab641eeceba5db77
b115030 <iitiangokul@gmail.com>
2020-10-30 11:03:36 +0530

    ADDED: Method and Test Cases to append elements to Linked List

---
+++ src/main/java/LinkedListUtils.java
@@ -0,0 +1,33 @@
+public class LinkedListUtils {
+    public INode head;
+    public INode tail;
+
+    public LinkedListUtils() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    // append node to the linked List
+    public void append(INode newNode) {
+        if (this.head == null)
+            this.head = newNode;
+        if (this.tail == null)
+            this.tail = newNode;
+        else {
+            this.tail.setNext(newNode);
+            this.tail = newNode;
+        }
+    }
+}
--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -25,12 +25,25 @@ public class MyLinkedListTest {
         MyLinkedList<Integer> thirdNode = new MyLinkedList<>(56);
         MyLinkedList<Integer> secondNode = new MyLinkedList<>(30);
         MyLinkedList<Integer> firstNode = new MyLinkedList<>(70);
-        HeadTailPlacement linkedList = new HeadTailPlacement();
+        LinkedListUtils linkedList = new LinkedListUtils();
         linkedList.add(firstNode);
         linkedList.add(secondNode);
         linkedList.add(thirdNode);
         boolean result = linkedList.head.equals(thirdNode) && linkedList.head.getNext().equals(secondNode)
                 && linkedList.tail.equals(firstNode);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenThreeElements_whenLinked_shouldBeAddedAtEnd() {
+        MyLinkedList<Integer> firstNode = new MyLinkedList<>(56);
+        MyLinkedList<Integer> secondNode = new MyLinkedList<>(30);
+        MyLinkedList<Integer> thirdNode = new MyLinkedList<>(70);
+        LinkedListUtils linkedList = new LinkedListUtils();
+        linkedList.append(firstNode);
+        linkedList.append(secondNode);
+        linkedList.append(thirdNode);
+        boolean result = linkedList.head.equals(firstNode) && linkedList.head.getNext().equals(secondNode)
+                && linkedList.tail.equals(thirdNode);
         Assert.assertTrue(result);
     }
 }
